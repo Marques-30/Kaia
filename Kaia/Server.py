2f94f69f54e5882febd5e08268af119311a65650
Marques Butilla <markship30@yahoo.com>
2020-10-13 21:47:47 -0700

    Add files via upload

--- Kaia/Server.py
+++ Kaia/Server.py
@@ -1,16 +1,18 @@
-import mysql.connector
+import sqlite3
 
-db_connection = mysql.connector.connect(
-  host= "localhost",
-  user= "root",
-  passwd= "root"
-  )
-# creating database_cursor to perform SQL operation
-db_cursor = db_connection.cursor()
-# executing cursor with execute method and pass SQL query
-db_cursor.execute("CREATE DATABASE my_first_db")
-# get list of all databases
-db_cursor.execute("SHOW DATABASES")
-#print all databases
-for db in db_cursor:
-    print(db)
+conn = sqlite3.connect('User_location.db')  # You can create a new database by changing the name within the quotes
+c = conn.cursor() # The database will be saved in the location where your 'py' file is saved
+
+# Create table - CLIENTS
+c.execute('''CREATE TABLE CLIENTS
+             ([generated_id] INTEGER PRIMARY KEY,[Client_Name] text, [phone] integer, [email] text)''')
+          
+# Create table - Flight
+c.execute('''CREATE TABLE Flights
+             ([generated_id] INTEGER PRIMARY KEY,[Client_Name] text, [Date_Time_Flight] text)''')
+        
+#Create table - Location
+c.execute('''CREATE TABLE Location
+             ([generated_id] INTEGER PRIMARY KEY,[Client_Name] text, [Location] text)''')
+                 
+conn.commit()
